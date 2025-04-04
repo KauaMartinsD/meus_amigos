6f842cc90e76f4fbd96c4a311660e71d4097765f
kaua <kauamartinsaraujo.1104@gmail.com>
2025-04-04 16:40:46 -0300

    fix: tocar musica no telefone

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const audio = document.getElementById("musica");
-  
-    const playMusic = () => {
-      audio.play();
-      document.removeEventListener('click', playMusic);
-      document.removeEventListener('touchstart', playMusic);
-    };
-  
-    document.addEventListener('click', playMusic);
-    document.addEventListener('touchstart', playMusic);
-  });
+  const audio = document.getElementById("musica");
+
+  const playMusic = () => {
+    audio.play().then(() => {
+      console.log("Música tocando.");
+    }).catch(err => {
+      console.log("Falha ao tocar música:", err);
+    });
+
+    // Remove os eventos depois do primeiro toque
+    document.removeEventListener('click', playMusic);
+    document.removeEventListener('touchstart', playMusic);
+  };
+
+  // Espera qualquer toque ou clique na tela
+  document.addEventListener('click', playMusic);
+  document.addEventListener('touchstart', playMusic);
+});
+
   
